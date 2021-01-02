21cc83cbedd263a6c6a31d229e8504a9e5ad7f65
Jaehoon Lee <jaehoonlee@Jaehoons-MacBook-Pro.local>
2021-01-02 18:44:39 +0900

    feat: solve the fifth problem out of 29

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ function evenOrOdd(num) {
     return 'Even'
 }
 
-console.log(evenOrOdd(2)); // Even
-console.log(evenOrOdd(3)); // Odd
-console.log(evenOrOdd(1000)); // Even
+// console.log(evenOrOdd(2)); // Even
+// console.log(evenOrOdd(3)); // Odd
+// console.log(evenOrOdd(1000)); // Even
+
 // Ternary operator
 function evenOrOdd(num) {
     return num % 2 ? 'Odd' : 'Even';
@@ -25,23 +26,23 @@ function getCount8 () {
     return count;
 }
 
-console.log(getCount8()); // 4000
+// console.log(getCount8()); // 4000
 
-// handling chars
+// 3. handling chars
 function alphaString46(s = '') {
     return (s.length < 4 || s.length > 7 || isNaN(s + '') || s === '') ? false : true;
 }
 
-console.log(alphaString46('1234')); // true
-console.log(alphaString46('9014')); // true
-console.log(alphaString46('723'));  // false
-console.log(alphaString46('a234')); // false
-console.log(alphaString46(''));     // false
-console.log(alphaString46());       // false
+// console.log(alphaString46('1234')); // true
+// console.log(alphaString46('9014')); // true
+// console.log(alphaString46('723'));  // false
+// console.log(alphaString46('a234')); // false
+// console.log(alphaString46(''));     // false
+// console.log(alphaString46());       // false
 
 //
 
-// 4 count 'p' and 'y' from the parameter
+// 4. count 'p' and 'y' from the parameter
 function numPY(s = '') {
     let countP = 0;
     let countY = 0;
@@ -54,9 +55,22 @@ function numPY(s = '') {
     return countY === countP ? true : false;
 }
 
-console.log(numPY('pPoooyY')); // true
-console.log(numPY('Pyy'));     // false
-console.log(numPY('ab'));      // true
-console.log(numPY(''));        // true
-console.log(numPY());          // true
+// console.log(numPY('pPoooyY')); // true
+// console.log(numPY('Pyy'));     // false
+// console.log(numPY('ab'));      // true
+// console.log(numPY(''));        // true
+// console.log(numPY());          // true
 
+// 5. create a weird text
+function toWeirdCase(s) {
+    let data = s.split('');
+    let start = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (!(start++ % 2)) data[i] = data[i].toUpperCase();      
+        if (data[i] === ' ') start = 0; 
+    }
+    return data.join(' ');
+}
+
+console.log(toWeirdCase('hello world'));    // 'HeLlO WoRlD'
+console.log(toWeirdCase('my name is lee')); // 'My NaMe Is LeE'
